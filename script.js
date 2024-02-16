2d1af0845ee0a7c8f58c082f61b579dac4ed128d
Marius Iordan <dopyno@yahoo.com>
2024-02-16 14:46:48 +0000

    2up

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict';
-const score0 = document.querySelector('#score--0');
-const score1 = document.getElementById('score1');
+
+//? Selecting elements LEC
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//? Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
